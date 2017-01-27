b63910fc7caf44584eeb73ea57a05b219e98739d
Brian Bycynski <high5bri@hotmail.com>
2017-01-27 07:18:07 -0500

    Final Project

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,9 @@ export default class Layout extends Component {
   render () {
     return <main>
       {this.props.children}
+      <div className='img2'>
+        <img src='http://del.h-cdn.co/assets/15/42/1600x800/landscape-1445028602-slow-cooker-chicken-wings-chipotle.jpg' />
+      </div>
       <footer>Made with care at The Iron Yard</footer>
     </main>
   }
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 // const MAPS_API_KEY = 'AIzaSyA4CnjqWeGHnG4FyA7_jn7KyHD5h_sQblY'
 // import data from '../data.json'
+import { Link } from 'react-router'
 
 export default class Restaurants extends Component {
   render () {
     return <div className='rest1'>
-      <h1>Hello Restaurants</h1>
+      <h1>Restaurants i enjoy</h1>
+      <button><Link to='/Header' />Home</button>
       {/* <div className='google-maps'>
         <iframe width='600' height='450' frameBorder='0' style={{border: 0}}
           src={`https://www.google.com/maps/embed/v1/place?q=place_id:${location.mapPlaceId}&key=${MAPS_API_KEY}`} allowFullScreen />
